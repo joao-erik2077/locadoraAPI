f41d8e274b466b63e4c617c6ff22884acdca79d7
joao-erik2077 <joaoerik2074@gmail.com>
2023-03-15 08:46:39 -0300

    Create V002__Insert.sql

---
+++ src/main/resources/db/migration/V002__Insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO ator(nomeator) VALUES ('João Catalão'), ('Pedrão Grandão'), ('Cebolácio Junior da Silva');
+INSERT INTO genero(descricao) VALUES ('comedia'), ('terror'), ('romance');
+
+INSERT INTO filmes(nomefilme, idator, idgenero) VALUES ('Cafeteira Assassina', 1, 1), ('Cafeteira Assassina II', 3, 2), ('Update V', 2, 3);
