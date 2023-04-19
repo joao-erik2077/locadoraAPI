57f9ff9042b71808beb71b7302b0d2bbe1296f55
joao-erik2077 <joaoerik2074@gmail.com>
2023-04-19 08:54:59 -0300

    resources

--- src/main/java/br/com/etec/joik/locadoraAPI/resource/FilmesResource.java
+++ src/main/java/br/com/etec/joik/locadoraAPI/resource/FilmesResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("filmes")
+@RequestMapping("/filmes")
 public class FilmesResource {
 
     @Autowired
--- src/main/java/br/com/etec/joik/locadoraAPI/resource/GeneroResource.java
+++ src/main/java/br/com/etec/joik/locadoraAPI/resource/GeneroResource.java
@@ -1,14 +1,24 @@
 package br.com.etec.joik.locadoraAPI.resource;
 
+import br.com.etec.joik.locadoraAPI.model.Genero;
 import br.com.etec.joik.locadoraAPI.repository.GeneroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("genero")
+@RequestMapping("/generos")
 public class GeneroResource {
 
     @Autowired
     private GeneroRepository generoRepository;
+
+    @GetMapping("/todos")
+    public List<Genero> listarTodosGeneros() {
+        return generoRepository.findAll(Sort.by("descricao").ascending());
+    }
 }
