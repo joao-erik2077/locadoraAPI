caf097179d5d57675fc4d71278318b051f763631
joao-erik2077 <joaoerik2074@gmail.com>
2023-04-26 08:24:07 -0300

    Create AtorFilter.java

---
+++ src/main/java/br/com/etec/joik/locadoraAPI/repository/filter/AtorFilter.java
@@ -0,0 +1,13 @@
+package br.com.etec.joik.locadoraAPI.repository.filter;
+
+public class AtorFilter {
+    private String nomeator;
+
+    public String getNomeator() {
+        return nomeator;
+    }
+
+    public void setNomeator(String nomeator) {
+        this.nomeator = nomeator;
+    }
+}
